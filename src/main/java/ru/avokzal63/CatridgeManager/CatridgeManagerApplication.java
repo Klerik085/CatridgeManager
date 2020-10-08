3077c4b8b5323f1190274a5c9e24b0561ca0d0e2
klerik <y.kislitsin85@gmail.com>
2020-10-08 09:42:33 +0400

    init

---
+++ src/main/java/ru/avokzal63/CatridgeManager/CatridgeManagerApplication.java
@@ -0,0 +1,13 @@
+package ru.avokzal63.CatridgeManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatridgeManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatridgeManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/avokzal63/CatridgeManager/CatridgeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.avokzal63.CatridgeManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatridgeManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
